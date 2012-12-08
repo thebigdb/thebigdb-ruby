4bf97be720382e9336cf1804932ba6398c1ea03d
Christophe Maximin <christophe@thebigdb.com>
2012-12-08 18:41:14 +0000

    added TheBigDB module spec

--- Guardfile
+++ Guardfile
@@ -3,5 +3,6 @@ guard 'rspec' do
   watch(%r{^spec/support/(.+)\.rb$}) { "spec" }
 
   watch(%r{^spec/.+_spec\.rb$})
+  watch(%r{^lib/thebigdb\.rb$}) { "spec" }
   watch(%r{^lib/thebigdb/(.+)\.rb$}) {|m| "spec/#{m[1]}_spec.rb" }
 end
---
+++ spec/thebigdb_spec.rb
@@ -0,0 +1,23 @@
+require "spec_helper"
+
+# Here's we're testing the basics of the module itself,
+# the variable that you can set, the actions you can take, etc.
+
+describe "TheBigDB module" do
+  it "has accessors for common used global variables" do
+    TheBigDB.api_host = "foobar"
+    TheBigDB.api_port = 1337
+    TheBigDB.api_version = "1337"
+
+    TheBigDB.api_host.should == "foobar"
+    TheBigDB.api_port.should == 1337
+    TheBigDB.api_version.should == "1337"
+  end
+
+  it "has resettable default values" do
+    default_host = TheBigDB.api_host.dup
+    TheBigDB.api_host = "foobar"
+    TheBigDB.reset_default_values
+    TheBigDB.api_host.should == default_host
+  end
+end
