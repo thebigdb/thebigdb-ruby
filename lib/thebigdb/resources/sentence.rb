351a35ddc17f0b0a1a81389110b65738c3ffb755
Christophe Maximin <christophe@thebigdb.com>
2013-02-23 03:15:58 +0000

    rebuilt Resources::Sentence

--- lib/thebigdb/resources/sentence.rb
+++ lib/thebigdb/resources/sentence.rb
@@ -1,42 +1,12 @@
 module TheBigDB
   module Resources
-    class Sentence
-      def self.get(id)
-        TheBigDB.send_request(:get, "/sentences", :id => id)
-      end
+    def self.Sentence(action, params)
+      method = ["get", "show", "search"].include?(action.to_s) ? "GET" : "POST"
+      path = "/sentences/#{action}"
 
-      def self.auto_complete(options = {})
-        # TODO
-      end
-
-      def self.search(options = {})
-        # TODO
-      end
-
-      def self.create(options = {})
-        # TODO
-      end
-
-      def self.upvote(options = {})
-        # TODO
-      end
-
-      def self.downvote(options = {})
-        # TODO
-      end
-
-      def self.report(options = {})
-        # TODO
-      end
-
-      def self.destroy(options = {})
-        # TODO
-      end
-
-      class << self
-        alias :autocomplete :auto_complete
-        alias :show :get
-      end
+      request = TheBigDB::Request.new
+      request.prepare(method, path, params)
+      request.execute
     end
   end
 end
--- spec/resources/sentence_spec.rb
+++ spec/resources/sentence_spec.rb
@@ -1,4 +1,29 @@
 require "spec_helper"
 
-# Tests all the actions to make sure they actually work in the basic sense that they execute.
-# Since they're ultimately going to return an un-treated Hash object, there's not much to test there.
+describe "Sentence" do
+  context "basic request executing" do
+    before do
+      stub_request(:get, /#{TheBigDB.api_host}\/v#{TheBigDB.api_version}\/sentences\/search/).to_return(:body => '{"server_says": "hello world"}')
+
+      @request = TheBigDB::Resources::Sentence(:search, nodes: ["a", "b"])
+    end
+
+    it "sets the correct data_sent instance variable" do
+      @request.data_sent.should == {
+          "headers" => Hash[@request.http_request.to_hash.map{|k,v| [k, v.join] }],
+          "host" => TheBigDB.api_host,
+          "port" => TheBigDB.api_port,
+          "path" => "/v#{TheBigDB.api_version}/sentences/search",
+          "method" => "GET",
+          "params" => {"nodes" => {"0" => "a", "1" => "b"}}
+        }
+    end
+
+    it "sets the correct data_received instance variable" do
+      @request.data_received.should include({
+          "headers" => Hash[@request.http_response.to_hash.map{|k,v| [k, v.join] }],
+          "content" => {"server_says" => "hello world"}
+        })
+    end
+  end
+end
