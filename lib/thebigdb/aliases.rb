abc2bc79b20c3819a15ba402ccb5d4ec177645d1
Christophe Maximin <christophe@thebigdb.com>
2013-02-28 23:10:47 +0000

    removed the alias "TBDB"

--- lib/thebigdb/aliases.rb
+++ lib/thebigdb/aliases.rb
@@ -2,7 +2,3 @@
 Thebigdb = TheBigDB
 TheBigDb = TheBigDB
 TheBigdb = TheBigDB
-
-# Because TheBigDB can be quite long to type, especially into an irb,
-# but please don't use that shortcut in production code, as it may disappear at any time.
-TBDB = TheBigDB
